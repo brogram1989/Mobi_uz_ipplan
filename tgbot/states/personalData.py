3501990d2e0a957d6aaad551d1c1812c357da778
Dilshod <133039838+brogram1989@users.noreply.github.com>
2024-09-16 07:04:33 +0500

    Update personalData.py
    Update personalData.py

--- tgbot/states/personalData.py
+++ tgbot/states/personalData.py
@@ -7,12 +7,13 @@ class AdminCommands(StatesGroup):
     del_admin = State()
 
 class FindId(StatesGroup):
-    send_ip_file = State() #ip faylni serverga yuborish stati
-    send_neid_file = State() #Hw nce filini serverga yuborish stati
-    get_updated_ip_file = State() #faylni serverdan qabul qilib olish stati
-    search_comand = State() #qidirishni bosganda ishga tushadigan state
-    input_bs_id = State() #yangi BS uchun ip adres olayotganda, bs idsi kiritilganda ishga tushadigan state
-    input_bs_name = State() #yangi BS uchun ip adres olayotganda, bs nomini kiritilganda ishga tushadigan state
-    send_msg = State() #userlarga message yuborish stati
+    send_ip_file = State() #ip faylni serverga yuborish xolati
+    send_neid_file = State() #Hw nce filini serverga yuborish xolati
+    get_updated_ip_file = State() #faylni serverdan qabul qilib olish xolati
+    search_comand = State() #qidirishni bosganda ishga tushadigan xolat
+    input_bs_id = State() #yangi BS uchun ip adres olayotganda, bs idsi kiritilganda ishga tushadigan xolat
+    input_bs_name = State() #yangi BS uchun ip adres olayotganda, bs nomini kiritilganda ishga tushadigan xolat
+    send_msg = State() #userlarga message yuborish xolati
+    check_ip = State() #ip faylini tekshirish xolati
 
 
